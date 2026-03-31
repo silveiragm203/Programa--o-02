39c19d34febb87b136419c94dbe88748d85f7526
Felipe Silveira <silveiragm203@gmail.com>
2026-03-31 11:52:33 -0300

    alteração dia 31

---
+++ Projeto Empréstimo.py
@@ -0,0 +1,31 @@
+Idade_Usuário = int(input("18"))
+salário_Usuário = float(input("2000"))
+Tempo= int(input("2"))
+
+if Idade_Usuário >= 18:
+    print("Apto para Empréstimo")
+elif Idade_Usuário >= 0 and Idade_Usuário < 18:
+    print("Empréstimo NEGADO")
+else:
+    print("NEGADO POR MENORIDADE")
+
+if salário_Usuário >= 2000:
+    print("Apto para Empréstimo")
+elif salário_Usuário < 2000:
+    print("Empréstimo Negado")
+elif salário_Usuário >= 5000:
+    print("Aprovação Automática")
+else:
+    print("Saldo Insuficiente")
+
+if Tempo >= 2:
+    print("Apto para Empréstimo")
+elif Tempo >= 0 and Tempo < 2:
+    print("Empréstimo negado")
+else:
+    print("Tempo Insuficiente")
+
+if Idade_Usuário >= 18 and salário_Usuário >= 2000 and Tempo >= 2:
+    print("Requisitos Para Empréstimo Aceito")
+elif Idade_Usuário < 18 and salário_Usuário >= 2000 and Tempo >= 2:
+    print("NEGADO POR MENORIDADE")
